4df97bfa06d043a9813c41eb2b8c1cab17f9fe9c
Jaunius Pinelis <jaunius@aml-analytics.com>
2021-01-01 12:09:39 +0200

    Continued with CatalogService

--- src/Catalog/Catalog.Api/Startup.cs
+++ src/Catalog/Catalog.Api/Startup.cs
@@ -1,15 +1,10 @@
+using Catalog.Api.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 
 namespace Catalog.Api
 {
@@ -26,6 +21,10 @@ namespace Catalog.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.Configure<CatalogDatabaseSettings>(Configuration.GetSection(nameof(CatalogDatabaseSettings)));
+
+            services.AddSingleton<ICatalogDatabaseSettings>(
+                sp => sp.GetRequiredService<IOptions<CatalogDatabaseSettings>>().Value);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

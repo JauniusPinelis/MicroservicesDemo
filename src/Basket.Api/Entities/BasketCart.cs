ee0d063dc01676525f605960d09a40729a065be5
Jaunius Pinelis <jaunius@aml-analytics.com>
2021-01-03 16:42:34 +0200

    Basket Entities Added

---
+++ src/Basket.Api/Entities/BasketCart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.Api.Entities
+{
+    public class BasketCart
+    {
+        public string UserName { get; set; }
+        public List<BasketCartItem> Items { get; set; } = new List<BasketCartItem>();
+
+        public BasketCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+    }
+}

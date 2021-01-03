eb1a054874b158b8e202bc9700ed79f975ddcb97
Jaunius Pinelis <jaunius@aml-analytics.com>
2021-01-03 17:00:05 +0200

    Redis initialised

---
+++ src/Basket.Api/Data/BasketContext.cs
@@ -0,0 +1,10 @@
+﻿using Basket.Api.Data.Interfaces;
+using StackExchange.Redis;
+
+namespace Basket.Api.Data
+{
+    public class BasketContext : IBasketContext
+    {
+        public IDatabase Redis => throw new System.NotImplementedException();
+    }
+}

cd91f3588b6b4af8a92f5fbf4eea41c91d4d5a6c
Jaunius Pinelis <jaunius@aml-analytics.com>
2021-01-06 17:55:11 +0200

    BasketRepository.cs

---
+++ src/Basket.Api/Repositories/BasketRepository.cs
@@ -0,0 +1,24 @@
+﻿using Basket.Api.Entities;
+using Basket.Api.Repositories.Interfaces;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        public Task<BasketCart> DeleteBasket(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<BasketCart> GetBasket(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<BasketCart> UpdateBasket(BasketCart basket)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Basket.Api/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.Api.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Repositories.Interfaces
+{
+    public interface IBasketRepository
+    {
+        public Task<BasketCart> GetBasket(string userName);
+        public Task<BasketCart> UpdateBasket(BasketCart basket);
+        public Task<BasketCart> DeleteBasket(string username);
+    }
+}

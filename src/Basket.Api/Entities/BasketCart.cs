ba9a92381736963e42a36e26dc139efe207b2522
Jaunius Pinelis <jaunius@aml-analytics.com>
2021-01-07 16:53:38 +0200

    Updates to docker and dockercompose

--- src/Basket.Api/Entities/BasketCart.cs
+++ src/Basket.Api/Entities/BasketCart.cs
@@ -7,6 +7,11 @@ namespace Basket.Api.Entities
         public string UserName { get; set; }
         public List<BasketCartItem> Items { get; set; } = new List<BasketCartItem>();
 
+        public BasketCart()
+        {
+
+        }
+
         public BasketCart(string userName)
         {
             UserName = userName;

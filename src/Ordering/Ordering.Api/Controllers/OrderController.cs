0704b261631e46fa4c5a57a3518de32a2da4c124
Jaunius Pinelis <jaunius@aml-analytics.com>
2021-01-16 15:02:40 +0200

    Ordering service started

---
+++ src/Ordering/Ordering.Api/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+
+    }
+}

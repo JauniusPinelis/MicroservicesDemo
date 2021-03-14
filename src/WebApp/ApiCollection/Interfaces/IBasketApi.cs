c5ef8e134558c521df8a7079933cb01261d8a399
Jaunius Pinelis <jaunius@aml-analytics.com>
2021-03-14 19:19:52 +0200

    Added Models and Api interfaces

---
+++ src/WebApp/ApiCollection/Interfaces/IBasketApi.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.ApiCollection.Interfaces
+{
+    public interface IBasketApi
+    {
+        Task<BasketModel> GetBasket(string userName);
+
+        Task<BasketModel> UpdateBasket(BasketModel model);
+
+        Task CheckoutBasket(BasketCheckoutModel model);
+    }
+}
---
+++ src/WebApp/ApiCollection/Interfaces/ICatalogApi.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.ApiCollection.Interfaces
+{
+    public interface ICatalogApi
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+
+        Task<CatalogModel> GetCatalog(string id);
+    }
+}
